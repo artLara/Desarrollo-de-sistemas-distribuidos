942639a57b6bb44ceebfaee4aa0a73c4d7b91c8d
artLara <art.lara.caz@gmail.com>
2020-12-21 09:54:07 -0600

    img tarea 9

--- Tarea9/client.sh
+++ Tarea9/client.sh
@@ -6,8 +6,8 @@ sudo apt install nfs-common
 
 # Crear el directorio de montaje en el cliente (punto de montaje):
 echo ">>>>>Crear el directorio de montaje en el cliente<<<<<"
-sudo mkdir -p /nfs/prueba
+sudo mkdir -p /nfs/cliente
 
 # Montar los directorios remotos (en este caso 40.87.94.140 es la IP del servidor):
 echo ">>>>>Montar los directorios remotos<<<<<"
-sudo mount -v -t nfs localhost:/var/nfs/prueba /nfs/prueba
+# sudo mount -v -t nfs 40.76.215.254:/var/nfs/servidor /nfs/cliente
--- Tarea9/server.sh
+++ Tarea9/server.sh
@@ -6,7 +6,7 @@ sudo apt install -y nfs-kernel-server
 
 # Crear el directorio compartido en el servidor;
 echo ">>>>>Directorio compartido<<<<<"
-sudo mkdir /var/nfs/prueba -p
+sudo mkdir /var/nfs/servidor -p
 
 # El propietario del directorio es root debido a que el directorio se creó con sudo, podemos ver el propietario ejecutando el comando:
 echo ">>>>>Propietario<<<<<"
@@ -15,8 +15,8 @@ ls -l /var/nfs
 # Debido a que NFS convierte el acceso del usuario root en el cliente en un acceso con el usuario "nobody:nogroup" en el servidor,
 # es necesario cambiar el propietario y permisos del directorio creado anteriormente:
 echo ">>>>>Cambio de propietario y permisos<<<<<"
-sudo chown nobody:nogroup /var/nfs/prueba
-sudo chmod 777 /var/nfs/prueba
+sudo chown nobody:nogroup /var/nfs/servidor
+sudo chmod 777 /var/nfs/servidor
 
 echo ">>>>>Verificar el nuevo propietario<<<<<"
 ls -l /var/nfs
@@ -24,8 +24,10 @@ ls -l /var/nfs
 # Ahora se debe registrar el directorio creado en el archivo de configuración de NFS.
 # Editar el archivo /etc/exports:
 echo ">>>>>Editar el archivo /etc/exports<<<<<"
+echo "====> Escribir /var/nfs/server hostname1(rw,sync,no_subtree_check) hostname2(rw,sync,no_subtree_check)"
 sudo vi /etc/exports
-# 6.2 Agregar la siguiente línea, guardar y salir del editor (en este caso 192.168.1.78 es la IP del cliente):
+# 6.2 Agregar la siguiente línea, guardar y salir del editor (en este caso 40.76.45.28 es la IP del cliente):
+# /var/nfs/prueba 40.76.45.28(rw,sync,no_subtree_check)
 
 #Actualizar la tabla de file systems exportados por NFS:
 echo ">>>>>Actualizar la tabla de file systems exportados por NFS<<<<<"
